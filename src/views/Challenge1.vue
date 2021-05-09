428de6afeeed0bd5cbbe8916b1e1730c9bfe919c
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-05-09 12:19:10 -0500

    feat: initial commit for spense challenge

--- src/views/Challenge1.vue
+++ src/views/Challenge1.vue
@@ -1,5 +1,11 @@
 <template>
     <main>
-        <div>challenge 1 page</div>
+        <div class="text-center bg-black text-white p-4">
+            <p class="mx-auto max-w-xl font-medium">
+                🎉 To celebreate Spense's launch, we will be waiving all fees for the entire month
+                of April.
+                <span class="underline">You will be receiving 100% of the earnings.</span> 🎉
+            </p>
+        </div>
     </main>
 </template>
