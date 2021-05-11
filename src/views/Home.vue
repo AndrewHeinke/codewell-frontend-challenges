e9a68ace145554f5dd1de6580d75f6ff3504fde8
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-05-10 21:29:36 -0500

    refactor: update color transitions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -136,61 +136,8 @@
 </template>
 
 <script>
-const colorOptions = [
-    {
-        bgColor: '#eeeeee',
-        shadowLight: '#ffffff',
-        shadowDark: '#cacaca',
-        textColor: '#6b7280',
-        linkColor: '#6d28d9',
-    },
-    {
-        bgColor: '#4A90E2',
-        shadowLight: '#55a6ff',
-        shadowDark: '#3f7ac0',
-        textColor: '#FFFFFF',
-        linkColor: '#F1BBDF',
-    },
-    {
-        bgColor: '#7ed321',
-        shadowLight: '#91F326',
-        shadowDark: '#6BB31C',
-        textColor: '#171738',
-        linkColor: '#3423A6',
-    },
-    {
-        bgColor: '#9013fe',
-        shadowLight: '#a616ff',
-        shadowDark: '#7A10D8',
-        textColor: '#BCF8EC',
-        linkColor: '#F9CFF2',
-    },
-    {
-        bgColor: '#f5a623',
-        shadowLight: '#FFBF28',
-        shadowDark: '#D08D1E',
-        textColor: '#1D2F6F',
-        linkColor: '#2E4BB2',
-    },
-    {
-        bgColor: '#3a3842',
-        shadowLight: '#43404c',
-        shadowDark: '#313038',
-        textColor: '#D8E8F6',
-        linkColor: '#44CF6C',
-    },
-];
-
-// function generateRandomColor(arr) {
-//     return arr[Math.floor(Math.random() * arr.length)];
-// }
-
-// function setSelectedIndex(s, i) {
-//     s.options[i - 1].selected = true;
-//     return;
-// }
-
 import { defineComponent } from 'vue';
+import { colorSchemes } from '../assets/color-schemes';
 
 export default defineComponent({
     mounted() {
@@ -209,7 +156,7 @@ export default defineComponent({
 
         function setStyles() {
             let colorIndex = myStorage.getItem('colorIndex');
-            let activeColor = colorOptions[colorIndex];
+            let activeColor = colorSchemes[colorIndex];
 
             document.getElementById('theme-select').value = colorIndex;
 
